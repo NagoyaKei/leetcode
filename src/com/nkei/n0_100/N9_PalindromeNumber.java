34e4d35919ce8b790f43636ddf2bfe87e160400c
Dmitriy Gorbunov <gorbunov@greendatasoft.ru>
2023-04-16 20:46:52 +0500

    add `9. Palindrome Number`

---
+++ src/com/nkei/n0_100/N9_PalindromeNumber.java
@@ -0,0 +1,20 @@
+package com.nkei.n0_100;
+
+public class N9_PalindromeNumber {
+    public boolean isPalindrome(int x) {
+        if (x < 0) {
+            return false;
+        }
+        char[] arr = (x + "").toCharArray();
+        int l = arr.length;
+        if (l <= 3 && arr[0] != arr[l - 1]) {
+            return false;
+        }
+        for (int i = 0; i < l / 2; i++) {
+            if (arr[i] != arr[l - i - 1]) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
---
+++ src/com/nkei/n0_100/test/N9_PalindromeNumberTest.java
@@ -0,0 +1,32 @@
+package com.nkei.n0_100.test;
+
+import com.nkei.n0_100.N9_PalindromeNumber;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class N9_PalindromeNumberTest {
+
+    @Test
+    void isPalindrome_121() {
+        N9_PalindromeNumber solution = new N9_PalindromeNumber();
+        var result = solution.isPalindrome(121);
+        assertTrue(result);
+    }
+
+    @Test
+    void isNotPalindrome_1231() {
+        N9_PalindromeNumber solution = new N9_PalindromeNumber();
+        var result = solution.isPalindrome(1231);
+        assertFalse(result);
+    }
+
+    @Test
+    void isPalindrome_one_digit() {
+        N9_PalindromeNumber solution = new N9_PalindromeNumber();
+        var result = solution.isPalindrome(1);
+        assertTrue(result);
+    }
+
+}
